cc1d8c021f8628a45e3043246cc922b26deb30e8
Samuel Kubai Kamau <samuel.kubai@andela.com>
2018-10-15 21:37:42 +0300

    feat(server-tunnel): configured the koding/tunnel package on the server

--- server/main.go
+++ server/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
   "fmt"
+  "net/http"
+  "github.com/koding/tunnel"
+  "os"
 )
 
 func main() {
-  fmt.Println("Mole server up and running...")
+  PORT := fmt.Sprintf(":%s", os.Getenv("PORT")) // ":5000"
+
+  // Initialize the tunnel server
+  cfg := &tunnel.ServerConfig{}
+  server, _ := tunnel.NewServer(cfg)
+  server.AddHost("tunnel.mole.test", "123456")
+
+  fmt.Println(fmt.Sprintf("Starting the tunnel server on tunnel.mole.test%s", PORT))
+  http.ListenAndServe(PORT, server)
 }
